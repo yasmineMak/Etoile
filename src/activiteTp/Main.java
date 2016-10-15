d17fd056a58a38f650fe9bb5a1a8e4ae4a17a9d4
YasMad <yasminemakreloufi@gmail.com>
2016-10-15 10:33:09 +0100

    j'ai crée le programme etoile

---
+++ src/activiteTp/Main.java
@@ -0,0 +1,22 @@
+package activiteTp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int espace;
+        String etoile="";
+        for (int i=0;i<10;i++)
+        {
+            espace=9-i;
+
+            for (int j=0;j<espace;j++)
+            {
+                System.out.print(" ");
+
+            }
+            etoile=etoile+"* ";
+            System.out.println(etoile);
+        }
+
+    }
+}
